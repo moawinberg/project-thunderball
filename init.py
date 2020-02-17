1918efece497e6f551e196409f278dd5eb1cea58
Moa <moa.winberg.1@gmail.com>
2020-02-17 13:19:25 +0100

    add python script to get zarr files

---
+++ init.py
@@ -0,0 +1,11 @@
+import s3fs
+import xarray as xr
+import pandas as pd
+
+credentials = pd.read_csv('./.aws/credentials.csv')
+fs = s3fs.S3FileSystem(key=credentials['Access key ID'][0], secret=credentials['Secret access key'][0])
+mapper = fs.get_mapper('greenlytics-meteorology-eu/NWP/NCEP/GFS/2020/02/13/00.zarr')
+ds = xr.open_zarr(mapper, consolidated=True)
+
+print ("hello world");
+print (ds);
