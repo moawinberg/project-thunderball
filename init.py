123a9095ba3182e26afa96e5464daee1562f6d7f
Moa <moa.winberg.1@gmail.com>
2020-02-17 13:36:11 +0100

    change mapper

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 credentials = pd.read_csv('./.aws/credentials.csv')
 fs = s3fs.S3FileSystem(key=credentials['Access key ID'][0], secret=credentials['Secret access key'][0])
-mapper = fs.get_mapper('greenlytics-meteorology-eu/NWP/NCEP/GFS/2020/02/13/00.zarr')
+mapper = fs.get_mapper('greenlytics-public/NWP/NCEP/GFS/2020/02/13/00.zarr')
 ds = xr.open_zarr(mapper, consolidated=True)
 
 print ("hello world");
