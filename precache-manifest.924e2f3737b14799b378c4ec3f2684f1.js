568906a6c0fa4e0bf0814a9c10ea1017ad34409d
Moa <moa.winberg.1@gmail.com>
2020-02-18 15:17:33 +0100

    Updates

---
+++ precache-manifest.924e2f3737b14799b378c4ec3f2684f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8789cf797886e53a60a36cf59e003354",
+    "url": "/project-thunderball/index.html"
+  },
+  {
+    "revision": "33bd095cdca2db8a572f",
+    "url": "/project-thunderball/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f998bbb687843c761fcd",
+    "url": "/project-thunderball/static/js/2.d36f95c0.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/project-thunderball/static/js/2.d36f95c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33bd095cdca2db8a572f",
+    "url": "/project-thunderball/static/js/main.ce5ee29d.chunk.js"
+  },
+  {
+    "revision": "7db3d4525bc3b5fb034a",
+    "url": "/project-thunderball/static/js/runtime-main.2aac282a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-thunderball/static/media/logo.5d5d9eef.svg"
+  }
+]);
