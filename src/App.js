6710a519227ff44ee80c5c5513e89874bd9edb97
Moa <moa.winberg.1@gmail.com>
2020-03-02 10:53:30 +0100

    install d3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapView from './Components/Map'
+import Timeline from './Components/Timeline'
 
 function App() {
   
 
   return (
     <div className="App">
+      <Timeline />
       <MapView />
     </div>
 
