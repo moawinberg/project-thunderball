8b751ad27fab6c49d1d4ec3c6df78b1ddc9dba25
Moa <moa.winberg.1@gmail.com>
2020-03-02 17:03:50 +0100

    fix merge error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useReducer } from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import MapView from './Components/Map'
 import useFetch from './lib/use-fetch';
-import * as qs from 'querystring'
-import create_polygons, { roundToQuarter } from './lib/form-polygons'
-import {range} from './lib/create_range';
+import create_polygons from './lib/form-polygons'
+import { range } from './lib/create_range';
 import tokens from './tokens.json'
+// import logo from './logo.svg';
+// import * as qs from 'querystring'
 
 const BASE_URL = "https://api.greenlytics.io/weather/v1";
 const REFERENCE_TIME = '2020-02-25 00';
 
-const bounds = {ne:{lon: 37, lat: 70}, sw: {lon:2, lat:52}}
+const bounds = { ne: { lon: 37, lat: 70 }, sw: { lon: 2, lat: 52 } }
 
-const coords = {lon:range(bounds.sw.lon, bounds.ne.lon), lat: range(bounds.sw.lat, bounds.ne.lat)}
+const coords = { lon: range(bounds.sw.lon, bounds.ne.lon), lat: range(bounds.sw.lat, bounds.ne.lat) }
 console.log(coords)
 
 function App() {
@@ -24,10 +24,10 @@ function App() {
     const endpoint_url = "/get_nwp?query_params="
     const params = {
       'model': 'DWD_ICON-EU',
-    'start_date': '2019-08-15 00',
-    'end_date': '2019-08-16 00',
-    'coords': {'latitude': coords.lat, 'longitude': coords.lon, 'height': [59,60]},
-    'variables': ['T']
+      'start_date': '2019-08-15 00',
+      'end_date': '2019-08-16 00',
+      'coords': { 'latitude': coords.lat, 'longitude': coords.lon, 'height': [59, 60] },
+      'variables': ['T']
     }
 
     const options = {
@@ -42,24 +42,15 @@ function App() {
 
   useEffect(() => {
     if (!isLoading && data !== null && error === null) {
-      setPolygons(create_polygons(data['data_vars']['T']['data'],coords.lon, coords.lat, 0, 0, 0))
+      setPolygons(create_polygons(data['data_vars']['T']['data'], coords.lon, coords.lat, 0, 0, 0))
     }
   }, [data, isLoading, error])
 
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-      {polygons ? (<MapView polygons={polygons}/>) : (<h3>Loading map..</h3>)}
-=======
-    	<SearchableMap />
-=======
->>>>>>> search/geolocation
+      {polygons ? (<MapView polygons={polygons} />) : (<h3>Loading map..</h3>)}
       <MapView />
->>>>>>> Add Search and geoLocate
     </div>
-
   );
 }
 
--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -3,126 +3,108 @@ import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css"
 import React, { useState, useEffect } from 'react';
 import MapGL, { NavigationControl, Source, Layer, GeolocateControl } from 'react-map-gl';
 import tokens from '../../tokens.json'
-import create_polygons, { roundToQuarter } from '../../lib/form-polygons'
-import { dataLayer } from './mapstyle'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Geocoder from "react-map-gl-geocoder";
+// import create_polygons, { roundToQuarter } from '../../lib/form-polygons'
+// import { dataLayer } from './mapstyle'
 
-const MapView = ({polygons}) => {
-    const mapRef = React.useRef();
-    const [polys, setPolys] = useState();
+const MapView = ({ polygons }) => {
+  const mapRef = React.useRef();
+  const [polys, setPolys] = useState();
 
-        const boundSW = [2.250475, 52.500440]; //longitude, latitude TWEAK THESE FOR BETTER BOUNDS
-        const boundNE = [37.848053, 70.740996];
-        
-    const [viewPort, setViewPort] = useState({
-        // just for prototyping purposes, change initial coords later
-        latitude: 59.33258,
-        longitude: 18.0649,
-        zoom: 5,
-        bearing: 0,
-        pitch: 0
-    });
+  const boundSW = [2.250475, 52.500440]; //longitude, latitude TWEAK THESE FOR BETTER BOUNDS
+  const boundNE = [37.848053, 70.740996];
 
-    const setBounds = (viewPort) => {
-            if (viewPort.longitude > boundNE[0]) {
-              viewPort.longitude = boundNE[0];
-            }
-            if (viewPort.longitude < boundSW[0]){
-                viewPort.longitude = boundSW[0];
-            }
-            if (viewPort.latitude > boundNE[1]){
-                viewPort.latitude = boundNE[1];
-            }
-            if (viewPort.latitude < boundSW[1]){
-                viewPort.latitude = boundSW[1];
-            }
-            
-            setViewPort(viewPort);
-          }
+  const [viewPort, setViewPort] = useState({
+    // just for prototyping purposes, change initial coords later
+    latitude: 59.33258,
+    longitude: 18.0649,
+    zoom: 5,
+    bearing: 0,
+    pitch: 0
+  });
 
-    // update map bounds when viewport changes
-    useEffect(() => {
-        if (mapRef) {
-         
-
-        }
-    }, [mapRef, viewPort])
-
-<<<<<<< HEAD
-    const dataLayer = {
-        id: 'data',
-        type: 'fill',
-        paint: {
-            'fill-color': [
-                'interpolate',
-                ['linear'],
-                ['get', 'airPressure'],
-                273,
-                '#03f4fc',
-                300,
-                '#fc0703'
-                ],
-            'fill-opacity': 0.5,
-          }
-        
+  const setBounds = (viewPort) => {
+    if (viewPort.longitude > boundNE[0]) {
+      viewPort.longitude = boundNE[0];
     }
-=======
+    if (viewPort.longitude < boundSW[0]) {
+      viewPort.longitude = boundSW[0];
+    }
+    if (viewPort.latitude > boundNE[1]) {
+      viewPort.latitude = boundNE[1];
+    }
+    if (viewPort.latitude < boundSW[1]) {
+      viewPort.latitude = boundSW[1];
+    }
 
-    const handleGeocoderViewportChange = viewport => {
+    setViewPort(viewPort);
+  }
+
+  // update map bounds when viewport changes
+  useEffect(() => {
+    if (mapRef) {
+
+
+    }
+  }, [mapRef, viewPort])
+
+  const dataLayer = {
+    id: 'data',
+    type: 'fill',
+    paint: {
+      'fill-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'airPressure'],
+        273,
+        '#03f4fc',
+        300,
+        '#fc0703'
+      ],
+      'fill-opacity': 0.5,
+    }
+  }
+
+  const handleGeocoderViewportChange = viewport => {
     const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
     return setViewPort({
       ...viewport,
       ...geocoderDefaultOverrides
     });
-    };
+  };
 
->>>>>>> search/geolocation
-
-    return (
-        <MapGL
-            {...viewPort}
-            width="100wv"
-            height="100vh"
-            onViewportChange={setBounds}
-            mapboxApiAccessToken={tokens["mapbox"]}
-            ref={mapRef}
-        >
-<<<<<<< HEAD
-<<<<<<< HEAD
-            {polygons && (<Source type="geojson" data={polygons}>
-                <Layer {...dataLayer} />
-            </Source>)}
-            <div style={{ "position": "absolute", "right": "0" }}>
-                <NavigationControl />
-=======
-            <div style={{"position": "absolute", "right": "0"}}>
-                <NavigationControl/>
-                <GeolocateControl
-                positionOptions={{enableHighAccuracy: true}}
-                trackUserLocation={true}
-                />
->>>>>>> Add Search and geoLocate
-            </div>
-=======
-        <Geocoder 
-            mapRef={ mapRef}
-            //onResult={this.handleOnResult}
-            onViewportChange={handleGeocoderViewportChange}
-            mapboxApiAccessToken = {tokens["mapbox"]}
-            position='top-left'
+  return (
+    <MapGL
+      {...viewPort}
+      width="100wv"
+      height="100vh"
+      onViewportChange={setBounds}
+      mapboxApiAccessToken={tokens["mapbox"]}
+      ref={mapRef}>
+      {
+        polygons && (
+          <Source type="geojson" data={polygons}>
+            <Layer {...dataLayer} />
+          </Source>
+        )
+      }
+      <div style={{ "position": "absolute", "right": "0" }}>
+        <NavigationControl />
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
         />
-        <div style={{"position": "absolute", "right": "0"}}>
-            <NavigationControl/>
-            <GeolocateControl
-            positionOptions={{enableHighAccuracy: true}}
-            trackUserLocation={true}
-            />
-        </div>
->>>>>>> search/geolocation
-        </MapGL>
-    )
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={tokens["mapbox"]}
+          position='top-left'
+        />
+      </div>
+    </MapGL>
+  )
 }
 
 export default MapView;
