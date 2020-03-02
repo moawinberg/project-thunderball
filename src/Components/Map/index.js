9ab34ae274545fb3f05b002757297e39cb866484
Mattias <matlar4@kth.se>
2020-03-02 13:19:47 +0100

    restict bounds hurra

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -7,6 +7,10 @@ import { dataLayer } from './mapstyle'
 const MapView = () => {
     const mapRef = React.useRef();
     const [polys, setPolys] = useState();
+
+        const boundSW = [2.250475, 52.500440]; //longitude, latitude TWEAK THESE FOR BETTER BOUNDS
+        const boundNE = [37.848053, 70.740996];
+        
     const [viewPort, setViewPort] = useState({
         // just for prototyping purposes, change initial coords later
         latitude: 59.33258,
@@ -15,6 +19,23 @@ const MapView = () => {
         bearing: 0,
         pitch: 0
     });
+
+    const setBounds = (viewPort) => {
+            if (viewPort.longitude > boundNE[0]) {
+              viewPort.longitude = boundNE[0];
+            }
+            if (viewPort.longitude < boundSW[0]){
+                viewPort.longitude = boundSW[0];
+            }
+            if (viewPort.latitude > boundNE[1]){
+                viewPort.latitude = boundNE[1];
+            }
+            if (viewPort.latitude < boundSW[1]){
+                viewPort.latitude = boundSW[1];
+            }
+            
+            setViewPort(viewPort);
+          }
 
     // update map bounds when viewport changes
     useEffect(() => {
@@ -44,6 +65,7 @@ const MapView = () => {
             },
             'fill-opacity': 0.5
           }
+        
     }
 
     return (
@@ -51,7 +73,7 @@ const MapView = () => {
             {...viewPort}
             width="100wv"
             height="100vh"
-            onViewportChange={setViewPort}
+            onViewportChange={setBounds}
             mapboxApiAccessToken={tokens["mapbox"]}
             ref={mapRef}
         >
