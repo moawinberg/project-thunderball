66e22d5757236c6b95a1f285cbad44f5c9b53b53
adelinaand <adelinaa@kth.se>
2020-03-02 12:43:31 +0100

    timeline

--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -20,7 +20,7 @@ import styles from './timeline.css'
 // }
 
 const Timeline = () => {
-  var lanes = ["Chinese", "Japanese", "Korean"],
+  var lanes = ["Chinese"],
     laneLength = lanes.length,
     items = [{ "lane": 0, "id": "Qin", "start": 5, "end": 205 },
     { "lane": 0, "id": "Jin", "start": 265, "end": 420 },
@@ -29,23 +29,7 @@ const Timeline = () => {
     { "lane": 0, "id": "Song", "start": 960, "end": 1265 },
     { "lane": 0, "id": "Yuan", "start": 1270, "end": 1365 },
     { "lane": 0, "id": "Ming", "start": 1370, "end": 1640 },
-    { "lane": 0, "id": "Qing", "start": 1645, "end": 1910 },
-    { "lane": 1, "id": "Yamato", "start": 300, "end": 530 },
-    { "lane": 1, "id": "Asuka", "start": 550, "end": 700 },
-    { "lane": 1, "id": "Nara", "start": 710, "end": 790 },
-    { "lane": 1, "id": "Heian", "start": 800, "end": 1180 },
-    { "lane": 1, "id": "Kamakura", "start": 1190, "end": 1330 },
-    { "lane": 1, "id": "Muromachi", "start": 1340, "end": 1560 },
-    { "lane": 1, "id": "Edo", "start": 1610, "end": 1860 },
-    { "lane": 1, "id": "Meiji", "start": 1870, "end": 1900 },
-    { "lane": 1, "id": "Taisho", "start": 1910, "end": 1920 },
-    { "lane": 1, "id": "Showa", "start": 1925, "end": 1985 },
-    { "lane": 1, "id": "Heisei", "start": 1990, "end": 1995 },
-    { "lane": 2, "id": "Three Kingdoms", "start": 10, "end": 670 },
-    { "lane": 2, "id": "North and South States", "start": 690, "end": 900 },
-    { "lane": 2, "id": "Goryeo", "start": 920, "end": 1380 },
-    { "lane": 2, "id": "Joseon", "start": 1390, "end": 1890 },
-    { "lane": 2, "id": "Korean Empire", "start": 1900, "end": 1945 }];
+    { "lane": 0, "id": "Qing", "start": 1645, "end": 1910 },];
 
   var timeBegin = 0;
   var timeEnd = 2000;
@@ -67,9 +51,6 @@ const Timeline = () => {
     .range([0, w]);
   var x1 = d3.scaleLinear()
     .range([0, w]);
-  var y1 = d3.scaleLinear()
-    .domain([0, laneLength])
-    .range([0, mainHeight]);
   var y2 = d3.scaleLinear()
     .domain([0, laneLength])
     .range([0, miniHeight]);
@@ -99,31 +80,12 @@ const Timeline = () => {
     .attr("class", "mini");
 
   //main lanes and texts
-  main.append("g").selectAll(".laneLines")
-    .data(items)
-    .enter().append("line")
-    .attr("x1", m[1])
-    .attr("y1", function (d) { return y1(d.lane); })
-    .attr("x2", w)
-    .attr("y2", function (d) { return y1(d.lane); })
-    .attr("stroke", "lightgray")
-
-  main.append("g").selectAll(".laneText")
-    .data(lanes)
-    .enter().append("text")
-    .text(function (d) { return d; })
-    .attr("x", -m[1])
-    .attr("y", function (d, i) { return y1(i + .5); })
-    .attr("dy", ".5ex")
-    .attr("text-anchor", "end")
-    .attr("class", "laneText");
 
   //mini lanes and texts
   mini.append("g").selectAll(".laneLines")
     .data(items)
     .enter().append("line")
     .attr("x1", m[1])
-    .attr("y1", function (d) { return y2(d.lane); })
     .attr("x2", w)
     .attr("y2", function (d) { return y2(d.lane); })
     .attr("stroke", "lightgray");
@@ -182,9 +144,7 @@ const Timeline = () => {
     rects.enter().append("rect")
       .attr("class", function (d) { return "miniItem" + d.lane; })
       .attr("x", function (d) { return x1(d.start); })
-      .attr("y", function (d) { return y1(d.lane) + 10; })
       .attr("width", function (d) { return x1(d.end) - x1(d.start); })
-      .attr("height", function (d) { return .8 * y1(1); });
 
     rects.exit().remove();
 
@@ -196,7 +156,6 @@ const Timeline = () => {
     labels.enter().append("text")
       .text(function (d) { return d.id; })
       .attr("x", function (d) { return x1(Math.max(d.start, minExtent)); })
-      .attr("y", function (d) { return y1(d.lane + .5); })
       .attr("text-anchor", "start");
 
     labels.exit().remove();
